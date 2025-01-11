6fad44b00224a2c89bb5aad716bd5787f8993050
Santiago Londoño Giraldo <slondonogi@unal.edu.co>
2025-01-10 21:31:23 -0500

    Done

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,56 @@ def pregunta_01():
 
 
     """
+    read_lines = open('./files/input/clusters_report.txt', 'r').readlines()
+    linea_procesada = [i.strip() for i in read_lines]
+
+    cont = []
+    for i in linea_procesada[2:]:
+        if i.strip():
+            data = i.split('\t')
+            cont.append(data)
+
+    def separate_numeric_and_text(list_of_lists):
+        result = []
+        for inner_list in list_of_lists:
+            string = inner_list[0]
+            numeric_part = re.findall(r'\d+(?:,\d+)*\.?\d*', string)
+            text_part = re.sub(r'\s+', ' ', re.sub(r'(?<!,)\d+(?:,\d+)*\.?\d*|[%]', '', string))
+
+            result.append((numeric_part, text_part))
+        return result
+
+    separated_lists = separate_numeric_and_text(data)
+
+    def create_dataframe(data):
+        rows = []
+        current_row = [None, None, None, ""]
+
+        for first_list, string in data:
+            if first_list:
+                # Si la primera lista no está vacía, creamos una nueva fila
+                if current_row[3]:
+                    rows.append(current_row.copy())
+                current_row = first_list + [string]
+            else:
+                # Si la primera lista está vacía, concatenamos el string a la fila actual
+                current_row[3] += ' ' + string
+
+        # Agregamos la última fila
+        rows.append(current_row)
+        # Creamos el DataFrame
+        df = pd.DataFrame(rows, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave',
+                                         'principales_palabras_clave'])
+        return df
+
+    df = create_dataframe(separated_lists[1:])
+
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('\s+', ' ')
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('.', '')
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.strip()
+    df['cluster'] = df['cluster'].astype(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',', '.')
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(float)
+
+    return df
